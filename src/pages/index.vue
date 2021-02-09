6622615727c1b80dba73d06cd1d40b93ee6885a6
Jeff <admin@jeffwilliams.dev>
2021-02-08 19:58:25 -0700

    MVP-79 wrench: setup

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
+      Test
       <card title="Free" icon="github">
         Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
       </card>
